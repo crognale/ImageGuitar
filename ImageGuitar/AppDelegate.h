11b071d5c8841260d75a68f4a5377e37ef641def
Sam Crognale <sam.crognale@gmail.com>
2015-02-21 15:32:46 -0800

    Initial Commit

---
+++ ImageGuitar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageGuitar
+//
+//  Created by Sam Crognale on 2/21/15.
+//  Copyright (c) 2015 Sam Crognale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImageGuitar/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ImageGuitar
+//
+//  Created by Sam Crognale on 2/21/15.
+//  Copyright (c) 2015 Sam Crognale. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
