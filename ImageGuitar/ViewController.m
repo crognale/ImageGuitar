7730b77c6b087fa3183d619a2aec7ecaa16458e0
Sam Crognale <sam.crognale@gmail.com>
2015-02-21 16:27:56 -0800

    camera screen w/ button

--- ImageGuitar/ViewController.m
+++ ImageGuitar/ViewController.m
@@ -7,9 +7,11 @@
 //
 
 #import "ViewController.h"
+#import <LLSimpleCamera/LLSimpleCamera.h>
 
 @interface ViewController ()
-
+@property (strong, nonatomic) LLSimpleCamera *camera;
+@property (strong, nonatomic) UIButton *snapButton;
 @end
 
 @implementation ViewController
@@ -17,6 +19,75 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    CGRect screenRect = [[UIScreen mainScreen] bounds];
+    
+    //create camera vc
+    self.camera = [[LLSimpleCamera alloc] initWithQuality:CameraQualityPhoto andPosition:CameraPositionBack];
+    
+    //attach to view
+    [self.camera attachToViewController:self withFrame:CGRectMake(0, 0, screenRect.size.width, screenRect.size.height)];
+
+    // capture
+    [self.camera capture:^(LLSimpleCamera *camera, UIImage *image, NSDictionary *metadata, NSError *error) {
+        if(!error) {
+            // we should stop the camera, since we don't need it anymore. We will open a new vc.
+            // this very important, otherwise you may experience memory crashes
+            [camera stop];
+            /*
+            // show the image
+            ImageViewController *imageVC = [[ImageViewController alloc] initWithImage:image];
+            [self presentViewController:imageVC animated:NO completion:nil];
+             */
+        }
+    }];
+    
+    //camera buttons
+    self.snapButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    self.snapButton.frame = CGRectMake(0, 0, 70.0f, 70.0f);
+    self.snapButton.clipsToBounds = YES;
+    self.snapButton.layer.cornerRadius = self.snapButton.frame.size.width / 2.0f;
+    self.snapButton.layer.borderColor = [UIColor whiteColor].CGColor;
+    self.snapButton.layer.borderWidth = 2.0f;
+    self.snapButton.backgroundColor = [[UIColor whiteColor] colorWithAlphaComponent:0.5];
+    self.snapButton.layer.rasterizationScale = [UIScreen mainScreen].scale;
+    self.snapButton.layer.shouldRasterize = YES;
+    [self.snapButton addTarget:self action:@selector(snapButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
+    
+
+    self.snapButton.center = self.view.center;
+    CGRect rect = self.snapButton.frame;
+    rect.origin = CGPointMake(rect.origin.x, self.view.frame.size.height - 15 - self.snapButton.frame.size.height);
+    self.snapButton.frame = rect;
+
+    
+        [self.view addSubview:self.snapButton];
+    
+}
+
+- (void)snapButtonPressed:(UIButton *)button {
+    
+    // capture
+    [self.camera capture:^(LLSimpleCamera *camera, UIImage *image, NSDictionary *metadata, NSError *error) {
+        if(!error) {
+            
+            // we should stop the camera, since we don't need it anymore. We will open a new vc.
+            // this very important, otherwise you may experience memory crashes
+            [camera stop];
+            
+            // show the image
+        //    ImageViewController *imageVC = [[ImageViewController alloc] initWithImage:image];
+          //  [self presentViewController:imageVC animated:NO completion:nil];
+        }
+    } exactSeenImage:YES];
+}
+
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    // start the camera
+    [self.camera start];
 }
 
 - (void)didReceiveMemoryWarning {
